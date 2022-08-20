24185dfafc4daab6f64de0ed1ceb2a1e51cf326b
flowseph1 <josephthenewandarmonius@gmail.com>
2022-08-19 20:39:15 -0600

    Change secret position

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -6,7 +6,7 @@ export default NextAuth({
         FacebookProvider({
             clientId: process.env.FACEBOOK_CLIENT_ID,
             clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
-            secret: process.env.NEXT_PUBLIC_SECRET,
         }),
     ],
+    secret: process.env.NEXT_PUBLIC_SECRET,
 });
